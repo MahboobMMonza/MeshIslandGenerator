64ba6d54b9f1d860d9972cf767b51e117ed64c58
Samih Dalvi <dalvis3@mcmaster.ca>
2023-02-21 14:30:14 -0500

    completed vertex interface creation

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/components/Vert.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.cas.se2aa4.a2.components;
+
+public interface Vert {
+
+    void setColour(int r, int g, int b, int a);
+
+    void setThickness(float t);
+
+    void setPosition(double x, double y);
+    
+}
