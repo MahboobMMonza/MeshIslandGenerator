ac47733b12db4fa8c03dd58b4209f4358f416e92
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-02-23 22:47:04 -0500

    Correctly render polygons

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -11,12 +11,15 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
-// import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-// import java.util.Set;
-// import java.util.TreeSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class GraphicRenderer {
 
@@ -26,25 +29,51 @@ public class GraphicRenderer {
     public void render(Mesh aMesh, Graphics2D canvas) {
         // mesh(aMesh, canvas);
         // squareLines(aMesh, canvas);
+        renderPolygons(aMesh, canvas);
         renderSegments(aMesh, canvas);
         renderVertices(aMesh, canvas);
     }
 
-    /* private class PointComparator implements Comparator<double[]> {
-        @Override
-        public int compare(double[] a, double[] b) {
-            int comp = Double.compare(a[0], b[0]);
-            return (comp != 0) ? comp : Double.compare(a[1], b[1]);
+    private class RadialVertexComparator implements Comparator<Vertex> {
+
+        private Vertex anchor;
+        static final double PRECISION = 1e-5;
+
+        RadialVertexComparator(Vertex anchor) {
+            setAnchor(anchor);
         }
-    } */
+
+        void setAnchor(Vertex anchor) {
+            this.anchor = anchor;
+        }
+
+        @Override
+        public int compare(Vertex v1, Vertex v2) {
+            // Determine if the orientation of these 2 points with the anchor is
+            // counterclockwise
+            // if it is, then v1 comes before v2
+            double angle1 = Math.atan2(v1.getY() - anchor.getY(), v1.getX() - anchor.getX()) + 0.0;
+            double angle2 = Math.atan2(v2.getY() - anchor.getY(), v2.getX() - anchor.getX()) + 0.0;
+            if (angle1 < 0) {
+                angle1 = Math.PI * 2 + angle1;
+            }
+            if (angle2 < 0) {
+                angle2 = Math.PI * 2 + angle2;
+            }
+            return Double.compare(angle1, angle2);
+        }
+    }
 
     private void renderSegments(Mesh aMesh, Graphics2D canvas) {
         canvas.setColor(Color.BLACK);
         canvas.setStroke(DEFAULT_STROKE);
         for (Segment s : aMesh.getSegmentsList()) {
-            Color old = canvas.getColor();
+            Color oldColor = canvas.getColor();
             Color strokeColor = extractColor(s.getPropertiesList());
+            Stroke oldStroke = canvas.getStroke();
+            Stroke stroke = extractThickness(s.getPropertiesList());
             canvas.setColor(strokeColor);
+            canvas.setStroke(stroke);
             Point2D p1 = new Point2D.Double(aMesh.getVertices(s.getV1Idx()).getX(),
                     aMesh.getVertices(s.getV1Idx()).getY());
             Point2D p2 = new Point2D.Double(aMesh.getVertices(s.getV2Idx()).getX(),
@@ -52,7 +81,8 @@ public class GraphicRenderer {
             Line2D line = new Line2D.Double(p1, p2);
             System.out.printf("[(%f, %f), (%f, %f)]%n", line.getX1(), line.getY1(), line.getX2(), line.getY2());
             canvas.draw(line);
-            canvas.setColor(old);
+            canvas.setColor(oldColor);
+            canvas.setStroke(oldStroke);
         }
     }
 
@@ -62,28 +92,54 @@ public class GraphicRenderer {
             double thickness = (double) extractVertexThickness(v.getPropertiesList());
             double centre_x = v.getX() - (thickness / 2.0d);
             double centre_y = v.getY() - (thickness / 2.0d);
-            Color old = canvas.getColor();
+            Color oldColor = canvas.getColor();
             canvas.setColor(extractColor(v.getPropertiesList()));
             if (thickness == 0.0 || canvas.getColor().getAlpha() == 0)
                 continue;
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, thickness, thickness);
             canvas.fill(point);
-            canvas.setColor(old);
+            canvas.setColor(oldColor);
         }
     }
 
-    private void renderPolygons(Mesh aMesh, Graphics2D canvas) {
-        canvas.setStroke(DEFAULT_STROKE);
-        // Set<double[]> points;
-        for (Polygon p : aMesh.getPolygonsList()) {
-            // points = new TreeSet<>(new PointComparator());
-            for (int segIdx : p.getSegmentIdxsList()) {
-                Vertex v1 = aMesh.getVertices(aMesh.getSegments(segIdx).getV1Idx());
-                Vertex v2 = aMesh.getVertices(aMesh.getSegments(segIdx).getV2Idx());
-                // points.add(new double[] { v1.getX(), v1.getY() });
-                // points.add(new double[] { v2.getX(), v2.getY() });
-            }
+    List<Vertex> calculatePolyPath(final Mesh aMesh, final Polygon p) {
+        // Sorting with these comparators should give an ordering bottom to top, left to
+        // right
+        Set<Vertex> pathVtx = new TreeSet<>(
+                Comparator.comparingDouble(Vertex::getX).thenComparing(Vertex::getY));
 
+        for (int segIdx : p.getSegmentIdxsList()) {
+            pathVtx.add(aMesh.getVertices(aMesh.getSegments(segIdx).getV1Idx()));
+            pathVtx.add(aMesh.getVertices(aMesh.getSegments(segIdx).getV2Idx()));
+        }
+        List<Vertex> path = new ArrayList<>(pathVtx);
+        Collections.sort(path, new RadialVertexComparator(aMesh.getVertices(p.getCentroidIdx())));
+        return path;
+    }
+
+    private void renderPolygons(Mesh aMesh, Graphics2D canvas) {
+        System.out.println("################################BEGIN POLYGON DRAWING################################");
+        canvas.setStroke(DEFAULT_STROKE);
+        Path2D poly;
+        boolean firstPoint;
+        for (Polygon p : aMesh.getPolygonsList()) {
+            System.out.println("-------------------------------- NEW POLYGON DRAWING --------------------------------");
+            poly = new Path2D.Double();
+            firstPoint = false;
+            List<Vertex> path = calculatePolyPath(aMesh, p);
+            for (Vertex v : path) {
+                if (!firstPoint) {
+                    poly.moveTo(v.getX(), v.getY());
+                    firstPoint = true;
+                } else {
+                    poly.lineTo(v.getX(), v.getY());
+                }
+            }
+            poly.closePath();
+            Color oldColor = canvas.getColor();
+            canvas.setColor(extractColor(p.getPropertiesList()));
+            canvas.fill(poly);
+            canvas.setColor(oldColor);
         }
     }
 
