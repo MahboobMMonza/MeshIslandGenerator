fa9eb0366e3f8a3666df4c8b8122fad0f4fea206
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-02-24 23:34:54 -0500

    Update generator Main method to conform to new generation practices

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,28 +1,34 @@
+import static ca.mcmaster.cas.se2aa4.a2.creator.MeshCreator.createMesh;
+
 import java.io.IOException;
 
+import ca.mcmaster.cas.se2aa4.a2.decorator.Decorator;
+import ca.mcmaster.cas.se2aa4.a2.decorator.FixedDecorator;
 import ca.mcmaster.cas.se2aa4.a2.generator.Generator;
 import ca.mcmaster.cas.se2aa4.a2.generator.GridGenerator;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.mesh.FixedMesh;
-import static ca.mcmaster.cas.se2aa4.a2.creator.MeshCreator.createMesh;
+import ca.mcmaster.cas.se2aa4.a2.mesh.Mesh;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FixedMesh fMesh = new FixedMesh();
         final int height = 500, width = 500, side = 20;
+        Mesh fMesh = new FixedMesh();
+        Decorator decorator = new FixedDecorator();
         Generator gen = new GridGenerator(side);
         MeshFactory factory = new MeshFactory();
         // Make vertices red
-        fMesh.setVertexColour(255, 0, 0, 255);
+        decorator.setVertColour("FF0000FF");
         // Make segments black
-        fMesh.setSegmentColour(0, 0, 0, 255);
-        // Make polygons yellow
-        fMesh.setPolygonColour(255, 255, 0, 255);
+        decorator.setSegColour("000000FF");
+        // Make polygons green
+        decorator.setPolyFillColour("00FF00FF");
         // Set vertex thickness to 3
-        fMesh.setVertexThickness(3.0f);
-        fMesh.setSegmentThickness(1.0f);
-        factory.write(createMesh(fMesh, gen, height, width), args[0]);
+        decorator.setVertThickness("3");
+        // Set segment thickness to 1
+        decorator.setSegThickness("1");
+        factory.write(createMesh(fMesh, gen, decorator, height, width), args[0]);
     }
 
 }
