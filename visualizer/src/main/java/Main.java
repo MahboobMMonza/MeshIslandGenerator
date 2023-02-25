ad3489b417e6f2af9acfc30588b88a24a6b82bc6
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-02-25 03:37:11 -0500

    Remove debug mode setting from GraphicsRenderer

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -23,7 +23,7 @@ public class Main {
         }
         // Creating the Canvas to draw the mesh
         Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
-        GraphicRenderer renderer = new GraphicRenderer(true);
+        GraphicRenderer renderer = new GraphicRenderer(false);
         // Painting the mesh on the canvas
         renderer.render(aMesh, canvas);
         // Storing the result in an SVG file
