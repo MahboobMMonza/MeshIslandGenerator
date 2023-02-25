e901fe9196378eaa0e9b5a0f3c8287f733778a7b
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-02-24 23:33:09 -0500

    Update MeshCreator to work with a given Decorator for decorating

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/creator/MeshCreator.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/creator/MeshCreator.java
@@ -4,6 +4,8 @@ import ca.mcmaster.cas.se2aa4.a2.generator.Generator;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import static ca.mcmaster.cas.se2aa4.a2.convertor.Convertor.convert;
 
+import ca.mcmaster.cas.se2aa4.a2.decorator.Decorator;
+
 /**
  * MeshCreator automates the process of creating a low-level io.Structs.Mesh
  * given a configured Mesh structure and a Generator with its required
@@ -12,9 +14,10 @@ import static ca.mcmaster.cas.se2aa4.a2.convertor.Convertor.convert;
 public class MeshCreator {
 
     public static Mesh createMesh(final ca.mcmaster.cas.se2aa4.a2.mesh.Mesh aMesh, final Generator gen,
-            final int height, final int width) {
+            final Decorator dec, final int height, final int width) {
         gen.generate(aMesh, height, width);
         aMesh.lock();
+        aMesh.decorateComponents(dec);
         return convert(aMesh);
     }
 }
