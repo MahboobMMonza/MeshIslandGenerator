6987af2dcdb70a1d5e9c58f6262ba8b6b392d27f
Samih Dalvi <dalvis3@mcmaster.ca>
2023-02-25 00:12:55 -0500

    added new colour for centroid

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -1,11 +1,9 @@
 package ca.mcmaster.cas.se2aa4.a2.visualizer;
-
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
-
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
@@ -20,9 +18,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-
 public class GraphicRenderer {
-
     private static final float DEFAULT_SEGMENT_THICKNESS = 2;
     private static final float DEFAULT_VERTEX_THICKNESS = 3;
     private static final float DEFAULT_POLYGON_THICKNESS = 0;
@@ -32,24 +28,20 @@ public class GraphicRenderer {
     private static final Color DEFAULT_SEGMENT_COLOR = Color.BLACK;
     private static final Color DEFAULT_VERTEX_COLOR = Color.RED;
     private static final Color DEFAULT_NEIGHBOR_COLOR = Color.WHITE;
+    private static final Color DEFAULT_CENTROID_COLOR = Color.BLUE;
     boolean debug = false;
 
     public GraphicRenderer(boolean debug) {
         this.debug = debug;
     }
-
     private class RadialVertexComparator implements Comparator<Vertex> {
-
         private Vertex anchor;
-
         RadialVertexComparator(Vertex anchor) {
             setAnchor(anchor);
         }
-
         void setAnchor(Vertex anchor) {
             this.anchor = anchor;
         }
-
         @Override
         public int compare(Vertex v1, Vertex v2) {
             // Determine if the orientation of these 2 points with the anchor is
@@ -66,11 +58,9 @@ public class GraphicRenderer {
             return Double.compare(angle1, angle2);
         }
     }
-
     public void setDebug(boolean debug) {
         this.debug = debug;
     }
-
     public boolean isDebug() {
         return debug;
     }
@@ -78,12 +68,10 @@ public class GraphicRenderer {
     public void render(Mesh aMesh, Graphics2D canvas) {
         renderPolygons(aMesh, canvas);
         renderSegments(aMesh, canvas);
-
         if (debug) {
             renderNeighbours(aMesh, canvas);
         }
         renderVertices(aMesh, canvas);
-
     }
 
     private void renderSegments(Mesh aMesh, Graphics2D canvas) {
@@ -106,7 +94,6 @@ public class GraphicRenderer {
             canvas.setStroke(oldStroke);
         }
     }
-
     private void renderVertices(Mesh aMesh, Graphics2D canvas) {
         canvas.setStroke(DEFAULT_STROKE);
         for (Vertex v : aMesh.getVerticesList()) {
@@ -170,18 +157,14 @@ public class GraphicRenderer {
                 }
                 Point2D p1 = new Point2D.Double(aMesh.getVertices(p.getCentroidIdx()).getX(),
                         aMesh.getVertices(p.getCentroidIdx()).getY());
-
                 Point2D p2 = new Point2D.Double(aMesh.getVertices(idx).getX(),
                         aMesh.getVertices(idx).getY());
-
                 Line2D line = new Line2D.Double(p1, p2);
                 canvas.draw(line);
-
             }
             visited.add(p.getCentroidIdx());
         }
     }
-
     private float extractThickness(final Vertex vert) {
         if (debug) {
             return DEFAULT_VERTEX_THICKNESS;
@@ -278,19 +261,16 @@ public class GraphicRenderer {
         int alpha = Integer.parseInt(raw[3]);
         return new Color(red, green, blue, alpha);
     }
-
+    
     private Color extractColor(Vertex vert) {
-        if (debug) {
-            return DEFAULT_VERTEX_COLOR;
-        }
-        String val = null;
+        String val = new String();
         for (Property p : vert.getPropertiesList()) {
             if (p.getKey().equals("rgba_color")) {
                 System.out.println(p.getValue());
                 val = p.getValue();
             }
         }
-        if (val == null) {
+        if (val.isEmpty()) {
             return DEFAULT_VERTEX_COLOR;
         }
         String[] raw = val.split(",");
@@ -298,7 +278,12 @@ public class GraphicRenderer {
         int green = Integer.parseInt(raw[1]);
         int blue = Integer.parseInt(raw[2]);
         int alpha = Integer.parseInt(raw[3]);
+        if (debug) {
+            if (alpha == 0) {
+                return DEFAULT_CENTROID_COLOR;
+            }
+            return DEFAULT_VERTEX_COLOR;
+        }
         return new Color(red, green, blue, alpha);
     }
-
 }
