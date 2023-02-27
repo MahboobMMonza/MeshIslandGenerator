a73df159c3e3ccf293d620e230552f3ede07eb8e
Samih Dalvi <dalvis3@mcmaster.ca>
2023-02-26 20:27:33 -0500

    fast mesh bounds determination

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.mcmaster.cas.se2aa4.a2.visualizer.GraphicRenderer;
 import ca.mcmaster.cas.se2aa4.a2.visualizer.MeshDump;
 import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvas;
@@ -8,6 +9,7 @@ import cli.VisualizerCommandLine;
 import java.awt.*;
 import java.io.IOException;
 
+import org.apache.commons.cli.AmbiguousOptionException;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 
@@ -23,16 +25,20 @@ public class Main {
         } else {
         String input= cmd.inputCli(parser, args);
         String output=cmd.outputCli(parser, args);
-        // Getting width and height for the canvas
+        // Getting width and width for the canvas
         Structs.Mesh aMesh = new MeshFactory().read(input);
-        double max_x = Double.MIN_VALUE;
-        double max_y = Double.MIN_VALUE;
-        for (Structs.Vertex v: aMesh.getVerticesList()) {
-            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
-            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
+        int height = 300, width = 300;
+        for(Property p: aMesh.getPropertiesList()){
+            if(p.getKey().equals("height")){
+                height = Integer.parseInt(p.getValue());
+            }
+            if(p.getKey().equals("width")){
+                width = Integer.parseInt(p.getValue());
+            }
         }
+
         // Creating the Canvas to draw the mesh
-        Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
+        Graphics2D canvas = SVGCanvas.build(width, height);
         GraphicRenderer renderer = new GraphicRenderer(cmd.hasDebugOption(parser, args));
         // Painting the mesh on the canvas
         renderer.render(aMesh, canvas);
