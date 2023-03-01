929c7857643384a72e61ef928312a6da01b0d3a9
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-01 01:41:18 -0500

    Added documentation to all component interfaces

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/components/Poly.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/components/Poly.java
@@ -4,30 +4,125 @@ import java.util.List;
 
 public interface Poly extends Comparable<Poly> {
 
+    /**
+     * Sets the centroid location of this Poly in 2D-space using double precision.
+     *
+     * @param x the X-coordintate of the centroid.
+     * @param y the Y-coordinate of the centroid.
+     */
     void setCentroid(double x, double y);
 
+    /**
+     * Sets the vertices of this Poly as a list of 2-element double arrays such that
+     * the first element stores the X-coordintate, and the second stores the
+     * Y-coordinate of the current vertex. The points must be stored such that each
+     *
+     * <p>
+     * point is adjacent to the one before it and the one after it, and the first
+     * and last points are adjacent to each other.
+     * </p>
+     *
+     * @param verticesList the list of vertex coordinates.
+     */
     void setVertices(List<double[]> verticesList);
 
+    /**
+     * Sets the centroids of the neighbouring Polys to this Poly as a list of
+     * 2-element double array such that the first element is the X-coordinate and
+     * the second element is the Y-coordinate of the centroid.
+     *
+     * @param neighbourCentroidList the list of neighbouring centroids of this Poly.
+     */
     void setNeighbours(List<double[]> neighbourCentroidList);
 
+    /**
+     * Sets the details for the fill colour of this Poly.
+     *
+     * <p>
+     * The values are stored in 8-bit RGBA colour value format. For the alpha value,
+     * the higher the value, the more opaque it is.
+     * </p>
+     *
+     * @param r the <em>red</em> value for the colour.
+     * @param g the <em>green</em> value for the colour.
+     * @param b the <em>blue</em> value for the colour.
+     * @param a the <em>alpha</em> (transparency) value for the colour.
+     */
     void setFillColour(int r, int g, int b, int a);
 
+    /**
+     * Sets the details for the border colour of this Poly.
+     *
+     * <p>
+     * The values are stored in 8-bit RGBA colour value format. For the alpha value,
+     * the higher the value, the more opaque it is.
+     * </p>
+     *
+     * @param r the <em>red</em> value for the colour.
+     * @param g the <em>green</em> value for the colour.
+     * @param b the <em>blue</em> value for the colour.
+     * @param a the <em>alpha</em> (transparency) value for the colour.
+     */
     void setBorderColour(int r, int g, int b, int a);
 
+    /**
+     * Sets the thickness of the border of this Poly.
+     *
+     * @param t the thickness value to set to.
+     */
     void setBorderThickness(float t);
 
+    /**
+     * Gets the list of 2-element double arrays storing the X and Y coordinates of
+     * this Poly's vertices.
+     *
+     * @return the list of vertex coordinates.
+     */
     List<double[]> getVertexList();
 
+    /**
+     * Gets the list of 2-element double arrays storing the X and Y coordinates of
+     * the centroids of the Poly which neighbours this Poly.
+     *
+     * @return the list of neighbouring centroid coordinates.
+     */
     List<double[]> getNeigbourList();
 
+    /**
+     * Gets the X-coordintate of this Poly's centroid.
+     *
+     * @return the X-coodrinate of the centroid.
+     */
     double getCentroidX();
 
+    /**
+     * Gets the Y-coordintate of this Poly's centroid.
+     *
+     * @return the Y-coodrinate of the centroid.
+     */
     double getCentroidY();
 
+    /**
+     * Gets the thickness of the borders of this Poly.
+     *
+     * @return the thickness.
+     */
     float getBorderThickness();
 
+    /**
+     * Gets the fill colour information of this Poly as a 4-element integer array,
+     * in RGBA order.
+     *
+     * @return the fill colour information of this Poly.
+     */
     int[] getFillColour();
 
+    /**
+     * Gets the border colour information of this Poly as a 4-element integer array,
+     * in RGBA order.
+     *
+     * @return the border colour information of this Poly.
+     */
     int[] getBorderColour();
 
 }
--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/components/Vert.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/components/Vert.java
@@ -2,21 +2,75 @@ package ca.mcmaster.cas.se2aa4.a2.components;
 
 public interface Vert extends Comparable<Vert> {
 
+    /**
+     * Sets the details of the colour of this Vert.
+     *
+     * <p>
+     * The values are stored in 8-bit RGBA colour value format. For the alpha value,
+     * the higher the value, the more opaque it is.
+     * </p>
+     *
+     * @param r the <em>red</em> value for the colour.
+     * @param g the <em>green</em> value for the colour.
+     * @param b the <em>blue</em> value for the colour.
+     * @param a the <em>alpha</em> (transparency) value for the colour.
+     */
     void setColour(int r, int g, int b, int a);
 
+    /**
+     * Gets the thickness of this Vert.
+     *
+     * @return the thickness.
+     */
     void setThickness(float t);
 
+    /**
+     * Sets the X and Y coordinate positions of this Vert in 2D-space with double
+     * precision.
+     *
+     * @param x the X-coordinate of this Vert.
+     * @param y the Y-coordinate of this Vert.
+     */
     void setPosition(double x, double y);
 
+    /**
+     * Gets the colour information of this Vert as a 4-element integer array,
+     * in RGBA order.
+     *
+     * @return the fill colour information of this Vert.
+     */
     int[] getColour();
 
+    /**
+     * Gets the X-coordinate of this Vert.
+     *
+     * @return the X-coordinate.
+     */
     double getX();
 
+    /**
+     * Gets the Y-coordinate of this Vert.
+     *
+     * @return the Y-coordinate.
+     */
     double getY();
 
+    /**
+     * Gets the thickness of this Vert.
+     *
+     * @return the thickness.
+     */
     float getThickness();
 
+    /**
+     * Sets this Vert as a centroid.
+     */
     void setCentroid();
 
+    /**
+     * Determines if this Vert is a centroid.
+     *
+     * @return whether this Vert is a centroid.
+     */
     boolean isCentroid();
 }
