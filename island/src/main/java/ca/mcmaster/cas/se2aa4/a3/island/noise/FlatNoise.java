586a2bff3712c01ed39b9576eeb6e8077a03b735
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-23 02:23:03 -0400

    F19: Non-modifying 'noise' for tiles

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/noise/FlatNoise.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.cas.se2aa4.a3.island.noise;
+
+import ca.mcmaster.cas.se2aa4.a3.island.components.ComponentCollections;
+import ca.mcmaster.cas.se2aa4.a3.island.components.Tile;
+import ca.mcmaster.cas.se2aa4.a3.island.components.TileTypes;
+
+/**
+ * FlatNoise does no modification on the original assignments
+ */
+public class FlatNoise implements NoiseTiler {
+
+    @Override
+    public void overwriteTileTypes(final ComponentCollections collection) {
+    }
+
+}
