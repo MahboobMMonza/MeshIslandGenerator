22dc88ab9d44167184835c378d6119a41ff2169a
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-23 14:39:22 -0400

    F06,F08: added new interfaces for lakes and aquifers

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/water/Aquifer.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.cas.se2aa4.a3.island.water;
+
+import ca.mcmaster.cas.se2aa4.a3.island.components.ComponentCollections;
+
+public interface Aquifer {
+
+    public void assignAquiferTiles(ComponentCollections collection, int num_aquifers, long seed);
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/water/Lake.java
@@ -0,0 +1,8 @@
+package ca.mcmaster.cas.se2aa4.a3.island.water;
+
+import ca.mcmaster.cas.se2aa4.a3.island.components.ComponentCollections;
+
+public interface Lake {
+
+    public void assignLakeTiles(ComponentCollections collection, int num_lakes, long seed);
+}
