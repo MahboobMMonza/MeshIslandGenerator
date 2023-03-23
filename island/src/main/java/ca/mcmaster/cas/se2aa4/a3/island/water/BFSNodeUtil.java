4c7b349d26653b2fb7db9f55dd7ae18aa49f8670
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-23 17:27:33 -0400

    F06: utility for lake propogation

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/water/BFSNodeUtil.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.cas.se2aa4.a3.island.water;
+
+public class BFSNodeUtil {
+    public final int INDEX, DIST;
+
+    public BFSNodeUtil(int index, int dist) {
+        this.INDEX = index;
+        this.DIST = dist;
+    }
+
+}
