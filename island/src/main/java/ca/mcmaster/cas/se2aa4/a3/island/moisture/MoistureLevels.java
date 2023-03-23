e581544f5ec90b4c2876f5d104ee9c2b8b68643d
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-23 05:24:13 -0400

    F09: add ID to each moisture level for better referencing

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/moisture/MoistureLevels.java
@@ -0,0 +1,14 @@
+public enum MoistureLevels {
+    OCEAN_MOISTURE(-3), LAGOON_MOISTURE(-2), WATER_MOISTURE(-1), DRY_MOISTURE(0),LOW_MOISTURE(1), HUMID_MOISTURE(2),
+    WET_MOISTURE(3);
+
+    public int getMoistureID() {
+        return moistureID;
+    }
+
+    private final int moistureID;
+
+    ElevationLevels(int moistureID) {
+        this.moistureID = moistureID;
+    }
+}
