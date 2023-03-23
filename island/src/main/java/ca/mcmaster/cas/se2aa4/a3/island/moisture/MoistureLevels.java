159fa7b3d0d00f80c4da7a0e63e4b18598d42a64
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-23 06:13:16 -0400

    added import statements

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/moisture/MoistureLevels.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/moisture/MoistureLevels.java
@@ -1,6 +1,8 @@
+package ca.mcmaster.cas.se2aa4.a3.island.moisture;
+
 public enum MoistureLevels {
-    OCEAN_MOISTURE(-3), LAGOON_MOISTURE(-2), WATER_MOISTURE(-1), DRY_MOISTURE(0),LOW_MOISTURE(1), HUMID_MOISTURE(2),
-    WET_MOISTURE(3);
+    OCEAN_MOISTURE(-3), LAGOON_MOISTURE(-2), WATER_MOISTURE(-1), DRY_MOISTURE(0),LOW_MOISTURE(1), 
+    HUMID_MOISTURE(2), WET_MOISTURE(3);
 
     public int getMoistureID() {
         return moistureID;
@@ -8,7 +10,7 @@ public enum MoistureLevels {
 
     private final int moistureID;
 
-    ElevationLevels(int moistureID) {
+    MoistureLevels(int moistureID) {
         this.moistureID = moistureID;
     }
 }
