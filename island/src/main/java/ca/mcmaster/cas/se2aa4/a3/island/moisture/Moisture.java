4d6c8534d4f95ff34e8b7f1e20c934943cc9a1a7
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-23 02:19:56 -0400

    F09: Moisture interface Define the interaction for assigning moisture levels to tiles.

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/moisture/Moisture.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.cas.se2aa4.a3.island.moisture;
+
+import ca.mcmaster.cas.se2aa4.a3.island.components.ComponentCollections;
+
+/**
+ * Moisture
+ */
+public interface Moisture {
+
+    public void moisturizeAllTiles(final ComponentCollections collection);
+}
