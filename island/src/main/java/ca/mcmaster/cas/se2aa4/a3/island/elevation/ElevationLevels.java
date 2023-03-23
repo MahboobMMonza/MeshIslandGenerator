33435ad44f5a67eaa8a39f403874d53dfd96f113
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-23 04:43:03 -0400

    F02: Add an ID to each elevation level For enhanced referencing since ints are better to work with.

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/ElevationLevels.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/ElevationLevels.java
@@ -4,6 +4,16 @@ package ca.mcmaster.cas.se2aa4.a3.island.elevation;
  * ElevationLevels
  */
 public enum ElevationLevels {
+    OCEAN_ELEVATION(-3), LAGOON_ELEVATION(-2), WATER_ELEVATION(-1), LOW_ELEVATION(0), MEDIUM_ELEVATION(1),
+    HIGH_ELEVATION(2);
 
-    OCEAN_ELEVATION, LAGOON_ELEVATION, WATER_ELEVATION, LOW_ELEVATION, MEDIUM_ELEVATION, HIGH_ELEVATION;
+    public int getElevationID() {
+        return elevationID;
+    }
+
+    private final int elevationID;
+
+    ElevationLevels(int elevationID) {
+        this.elevationID = elevationID;
+    }
 }
