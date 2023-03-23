3a488160f6a8afc102e03f0de9a529e9527d6297
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-23 04:21:14 -0400

    F02: Creating standardized elevation mappings Create a standard set of elevations that all elevatio profiles must use.

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/ElevationLevels.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.cas.se2aa4.a3.island.elevation;
+
+/**
+ * ElevationLevels
+ */
+public enum ElevationLevels {
+
+    OCEAN_ELEVATION, LAGOON_ELEVATION, WATER_ELEVATION, LOW_ELEVATION, MEDIUM_ELEVATION, HIGH_ELEVATION;
+}
