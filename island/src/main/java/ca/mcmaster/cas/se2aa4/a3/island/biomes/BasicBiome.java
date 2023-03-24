8628e5b8a05bf9cf37cdd8bdd47bde702b89c8aa
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-23 23:40:26 -0400

    F06: updating biome to work with lakes

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/biomes/BasicBiome.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/biomes/BasicBiome.java
@@ -10,8 +10,8 @@ import ca.mcmaster.cas.se2aa4.a3.island.elevation.ElevationLevels;
  */
 public class BasicBiome implements Biome {
 
-    public static final int OCEAN_COLOUR = 0xFF000046, WATER_COLOUR = 0xFF354CDE,
-            SHORE_COLOUR = 0xFFFBEAB5, LAND_COLOUR = 0xFFFFFFED;
+    public static final int OCEAN_COLOUR = 0xFF2140A3, LAGOON_COLOUR = 0xFF2488D5,
+            SHORE_COLOUR = 0xFFFBEAB5, LAND_COLOUR = 0xFFFFFFED, WATER_COLOUR = 0xFF1AA9D0;
 
     @Override
     public void assignBiomes(ComponentCollections collection) {
@@ -20,14 +20,16 @@ public class BasicBiome implements Biome {
             if (tile.getElevation() == ElevationLevels.OCEAN_ELEVATION) {
                 tile.setColour(OCEAN_COLOUR);
             } else if (tile.getElevation() == ElevationLevels.LAGOON_ELEVATION) {
-                tile.setColour(WATER_COLOUR);
+                tile.setColour(LAGOON_COLOUR);
             } else if (tile.getElevation() == ElevationLevels.LOW_ELEVATION) {
                 tile.setColour(SHORE_COLOUR);
+            } else if (tile.getElevation() == ElevationLevels.WATER_ELEVATION) {
+                tile.setColour(WATER_COLOUR);
             }
         }
 
         for (Edge edge : collection.getAllEdges().values()) {
-            edge.setColour(WATER_COLOUR);
+            edge.setColour(LAGOON_COLOUR);
         }
     }
 
