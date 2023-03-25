f1bb16c0918d126ad873944cc4fa3cc85bce1a84
Khalid <faragk1@mcmaster.ca>
2023-03-25 18:13:33 -0400

    F19: Modifiable IslandCreator Implemented

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/creator/IslandCreator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/creator/IslandCreator.java
@@ -7,13 +7,31 @@ import static ca.mcmaster.cas.se2aa4.a3.island.convertor.Convertor.*;
 import ca.mcmaster.cas.se2aa4.a3.island.elevation.*;
 import ca.mcmaster.cas.se2aa4.a3.island.moisture.*;
 import ca.mcmaster.cas.se2aa4.a3.island.shaper.*;
+import ca.mcmaster.cas.se2aa4.a3.island.water.aquifer.*;
+import ca.mcmaster.cas.se2aa4.a3.island.water.lake.*;
 
 /**
  * IslandCreator
  */
 public class IslandCreator {
 
-    public static Mesh createIsland(final Mesh mesh, final int height, final int width, final long seed) {
+    private Lake lake;
+    private Aquifer aquifer;
+    private Elevation elevation;
+    private Moisture moisture;
+    private Biome biome;
+    private ShapeFilter shape;
+
+    IslandCreator(Lake lake, Aquifer aquifer, Elevation elevation, Moisture moisture, Biome biome, ShapeFilter shape) {
+        this.lake = lake;
+        this.aquifer = aquifer;
+        this.elevation = elevation;
+        this.moisture = moisture;
+        this.biome = biome;
+        this.shape = shape;
+    }
+
+    public Mesh createIsland(final Mesh mesh, final int height, final int width, final long seed) {
         ShapeFilter shape = new LagoonShaper(height, width, seed);
         ComponentCollections collection = ComponentCollections.COLLECTION;
         Elevation elev = new BasicElevation();
