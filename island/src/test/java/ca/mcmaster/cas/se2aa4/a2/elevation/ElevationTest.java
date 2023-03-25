056371b3a2aa33188dc44ba5d33b0c1a6d9f0855
Khalid <faragk1@mcmaster.ca>
2023-03-25 15:07:31 -0400

    F02, F09: Created test cases for elevation and moisture

---
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a2/elevation/ElevationTest.java
@@ -0,0 +1,40 @@
+package ca.mcmaster.cas.se2aa4.a2.elevation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import ca.mcmaster.cas.se2aa4.a3.island.elevation.*;
+
+public class ElevationTest {
+
+    @Test
+    public void testNormalAssignLevel() {
+        NormalElevation elev = new NormalElevation();
+        Map<Double, ElevationLevels> ranges = elev.getElevationRanges();
+
+        assertEquals(ElevationLevels.OCEAN_ELEVATION, ranges.get(-3.0));
+        assertEquals(ElevationLevels.LAGOON_ELEVATION, ranges.get(-2.0));
+        assertEquals(ElevationLevels.WATER_ELEVATION, ranges.get(-1.0));
+        assertEquals(ElevationLevels.LOW_ELEVATION, ranges.get(1.0 / 3.0));
+        assertEquals(ElevationLevels.MEDIUM_ELEVATION, ranges.get(2.0 / 3.0));
+        assertEquals(ElevationLevels.HIGH_ELEVATION, ranges.get(Double.MAX_VALUE));
+    }
+
+    @Test
+    public void testSteepAssignLevel() {
+        SteepElevation elevation = new SteepElevation();
+        Map<Double, ElevationLevels> ranges = elevation.getElevationRanges();
+
+        assertEquals(ElevationLevels.OCEAN_ELEVATION, ranges.get(-3.0));
+        assertEquals(ElevationLevels.LAGOON_ELEVATION, ranges.get(-2.0));
+        assertEquals(ElevationLevels.WATER_ELEVATION, ranges.get(-1.0));
+        assertEquals(ElevationLevels.LOW_ELEVATION, ranges.get(0.65));
+        assertEquals(ElevationLevels.MEDIUM_ELEVATION, ranges.get(0.84));
+        assertEquals(ElevationLevels.HIGH_ELEVATION, ranges.get(Double.MAX_VALUE));
+    }
+}
