9f425b13a58eec8d69b885d6298446a59d137792
Khalid <faragk1@mcmaster.ca>
2023-03-25 19:10:06 -0400

    F19: Updated main file to incorprate all the options, and utilizes the factories

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -2,7 +2,14 @@ import java.io.*;
 
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.*;
-import static ca.mcmaster.cas.se2aa4.a3.island.creator.IslandCreator.*;
+import ca.mcmaster.cas.se2aa4.a3.island.creator.*;
+import ca.mcmaster.cas.se2aa4.a3.island.biomes.*;
+import ca.mcmaster.cas.se2aa4.a3.island.shaper.*;
+import ca.mcmaster.cas.se2aa4.a3.island.moisture.*;
+import ca.mcmaster.cas.se2aa4.a3.island.elevation.*;
+import ca.mcmaster.cas.se2aa4.a3.island.water.lake.*;
+import ca.mcmaster.cas.se2aa4.a3.island.water.aquifer.*;
+
 import static ca.mcmaster.cas.se2aa4.a3.island.preprocessor.MeshPreprocessor.*;
 
 import cli.IslandCommandLine;
@@ -19,21 +26,64 @@ public class Main {
     public static void main(String[] args) throws IOException {
         CommandLineParser parser = new DefaultParser();
         IslandCommandLine cmd = new IslandCommandLine();
-        cmd.addOptions();
+        IslandCreatorBuilder builder = new IslandCreatorBuilder();
+        AbstractBiomeFactory biomeFactory;
+        AbstractAquiferFactory aquiferFactory;
+        AbstractLakeFactory lakeFactory;
+        AbstractElevationFactory elevationFactory;
+        AbstractMoistureFactory moistureFactory;
+        AbstractShaperFactory shapeFilterFactory;
+        int numOfLakes;
+        int numOfAquifers;
+        String input;
+        String output;
+        long seed;
+        int height;
+        int width;
+        Mesh inputMesh;
+        SoilTypes soilType;
         MeshFactory factory = new MeshFactory();
+        cmd.addOptions();
         ModeTypes type = cmd.getModeType(parser, args);
-        if (cmd.hasHelpOption(parser, args) || type.equals(ModeTypes.NONE)) {
+        boolean help = cmd.hasHelpOption(parser, args);
+
+        if (type.equals(ModeTypes.NONE)) {
+            if (!help) {
+                System.out.println("WARNING: A valid mode type was not provided.");
+            }
             cmd.getHelp();
             System.exit(0);
         }
-        String input = cmd.inputCli(parser, args);
-        String output = cmd.outputCli(parser, args);
-        long seed = cmd.getSeed(parser, args);
-        Mesh inputMesh = factory.read(input);
-        int height = getHeight(inputMesh), width = getWidth(inputMesh);
+        input = cmd.inputCli(parser, args);
+        output = cmd.outputCli(parser, args);
+        seed = cmd.getSeed(parser, args);
+        inputMesh = factory.read(input);
+        height = getHeight(inputMesh);
+        width = getWidth(inputMesh);
+        soilType = cmd.getSoilType(parser, args);
+        numOfLakes = cmd.getNumOfLakes(parser, args);
+        numOfAquifers = cmd.getNumOfAquifers(parser, args);
+        if (type.equals(ModeTypes.LAGOON)) {
+            biomeFactory = new BasicBiomeFactory();
+            moistureFactory = new BasicMoistureFactory();
+            elevationFactory = new BasicElevationFactory();
+            aquiferFactory = new BasicAquiferFactory();
+            lakeFactory = new BasicLakeFactory();
+            shapeFilterFactory = new LagoonShaperFactory();
+            builder.biome(biomeFactory.createBiome())
+                    .moisture(moistureFactory.createMoisture(soilType))
+                    .elevation(elevationFactory.createElevationProfile())
+                    .aquifer(aquiferFactory.createAquifer(seed, numOfAquifers))
+                    .lake(lakeFactory.createLake(seed, numOfLakes))
+                    .shape(shapeFilterFactory.createShaper(height, width, seed));
+        } else {
+            System.exit(0);
+        }
         System.out.println("Begin island creation");
         System.out.println("The seed used is: " + seed);
-        factory.write(createIsland(inputMesh, height, width, seed), output);
+        // Add the necessary options to the builder
+        IslandCreator island = builder.build();
+        factory.write(island.createIsland(inputMesh), output);
         System.out.println("The file is stored as: " + output);
     }
 
