2221a69fcb3bc39fcdaa21ce9b592aa8c4200117
Samih Dalvi <dalvis3@mcmaster.ca>
2023-03-26 08:28:47 -0400

    F02: added elevation modifier

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/SteepElevation.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/SteepElevation.java
@@ -4,17 +4,11 @@ import java.util.*;
 
 public class SteepElevation extends NormalElevation {
 
+    public static final double ELEVATION_MODIFIER = 0.75;
+
     @Override
-    public Map<Double, ElevationLevels> getElevationRanges() {
-        // This map is pseudo constant but declared this way for inheritance
-        Map<Double, ElevationLevels> ELEVATION_RANGES = new TreeMap<>();
-        ELEVATION_RANGES.put(-3.0, ElevationLevels.OCEAN_ELEVATION);
-        ELEVATION_RANGES.put(-2.0, ElevationLevels.LAGOON_ELEVATION);
-        ELEVATION_RANGES.put(-1.0, ElevationLevels.WATER_ELEVATION);
-        ELEVATION_RANGES.put(0.65, ElevationLevels.LOW_ELEVATION);
-        ELEVATION_RANGES.put(0.84, ElevationLevels.MEDIUM_ELEVATION);
-        ELEVATION_RANGES.put(Double.MAX_VALUE, ElevationLevels.HIGH_ELEVATION);
-        return ELEVATION_RANGES;
+    double calculateDistanceDistribution(double x1, double y1, double x2, double y2) {
+        return Math.pow((Math.pow(x2 - x1, 2.0) + Math.pow(y2 - y1, 2.0)), ELEVATION_MODIFIER);
     }
 
 }
