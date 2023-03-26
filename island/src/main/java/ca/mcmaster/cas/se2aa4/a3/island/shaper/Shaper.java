2a2d668c2b5d3de716de7b0b7252718641f144c8
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-26 02:40:10 -0400

    F19: Rename shape filter to shaper.

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/shaper/Shaper.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/shaper/Shaper.java
@@ -6,7 +6,7 @@ import ca.mcmaster.cas.se2aa4.a3.island.components.*;
 /**
  * ShapeFilter gives a value to subtract from some base noise
  */
-public interface ShapeFilter {
+public interface Shaper {
 
     public Map<Integer, TileTypes> shapeAllTiles(final ComponentCollections collection);
 }
