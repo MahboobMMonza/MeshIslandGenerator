03c4a161ef4e3f3df9c7eb9db540134070ee90a2
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-26 10:30:11 -0400

    F13: Generalize Nodeutil to a generic pair utility
    This is to help with return types for river methods.

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/water/PairUtil.java
@@ -0,0 +1,12 @@
+package ca.mcmaster.cas.se2aa4.a3.island.water;
+
+public class PairUtil<T, G> {
+    public final T FIRST;
+    public final G SECOND;
+
+    public PairUtil(T first, G second) {
+        this.FIRST = first;
+        this.SECOND = second;
+    }
+
+}
