3608c2a3312b3603a03897c8d23ce02cb7d53414
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-03-26 22:10:30 -0400

    F19: Clearer messages for user

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -96,9 +96,9 @@ public class Main {
                 .river(new River(seed, numOfRivers));
         System.out.println("Begin island creation");
         System.out.println("The seed used is: " + seed);
-        System.out.println("The number of lakes is: " + numOfLakes);
-        System.out.println("The number of aquifers is: " + numOfAquifers);
-        System.out.println("The number of rivers is: " + numOfRivers);
+        System.out.println("The maximum number of lakes is: " + numOfLakes);
+        System.out.println("The maximum number of aquifers is: " + numOfAquifers);
+        System.out.println("The maximum number of rivers is: " + numOfRivers);
         System.out.println("The input file is: " + input);
         // Add the necessary options to the builder
         IslandCreator island = builder.build();
