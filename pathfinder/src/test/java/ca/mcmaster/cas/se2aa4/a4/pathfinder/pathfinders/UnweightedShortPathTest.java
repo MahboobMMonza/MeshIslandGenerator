f6c5889684a4fedbda716776af7fb41d5e9ef5cd
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-04-12 16:02:19 -0400

    F03: TEST Change confirmation list creation method

--- pathfinder/src/test/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/pathfinders/UnweightedShortPathTest.java
+++ pathfinder/src/test/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/pathfinders/UnweightedShortPathTest.java
@@ -28,12 +28,12 @@ public class UnweightedShortPathTest {
         edges.add(new BasicBiEdge<Integer>(4, 4, 2, 1));
         edges.add(new BasicBiEdge<Integer>(5, 4, 3, 1));
         edges.add(new BasicBiEdge<Integer>(6, 4, 5, 1));
-        adj.add(Arrays.asList(0, 1));
-        adj.add(Arrays.asList(0, 2));
-        adj.add(Arrays.asList(2, 3, 4));
-        adj.add(Arrays.asList(1, 3, 5));
-        adj.add(Arrays.asList(4, 5, 6));
-        adj.add(Arrays.asList(6));
+        adj.add(List.of(0, 1));
+        adj.add(List.of(0, 2));
+        adj.add(List.of(2, 3, 4));
+        adj.add(List.of(1, 3, 5));
+        adj.add(List.of(4, 5, 6));
+        adj.add(List.of(6));
         graph.setAllNodes(nodes);
         graph.setAllEdges(edges);
         for (int i = 0; i < adj.size(); i++) {
@@ -41,13 +41,13 @@ public class UnweightedShortPathTest {
         }
         Pathfinder<Integer, Integer> pathfinder = new UnweightedShortPath<Integer>();
         PathInfoTriple<Integer> info = pathfinder.findAllSourcePaths(graph, 0);
-        assertEquals(Arrays.asList(0, 1, 2, 1, 2, 3), info.getPathCosts());
-        assertEquals(Arrays.asList(), info.getPathEdgeIdxs().get(0));
-        assertEquals(Arrays.asList(0), info.getPathEdgeIdxs().get(1));
-        assertTrue(info.getPathEdgeIdxs().get(2).equals(Arrays.asList(0, 2))
-                || info.getPathEdgeIdxs().get(2).equals(Arrays.asList(1, 3)));
-        assertEquals(Arrays.asList(1), info.getPathEdgeIdxs().get(3));
-        assertEquals(Arrays.asList(1, 5), info.getPathEdgeIdxs().get(4));
+        assertEquals(List.of(0, 1, 2, 1, 2, 3), info.getPathCosts());
+        assertEquals(List.of(), info.getPathEdgeIdxs().get(0));
+        assertEquals(List.of(0), info.getPathEdgeIdxs().get(1));
+        assertTrue(info.getPathEdgeIdxs().get(2).equals(List.of(0, 2))
+                || info.getPathEdgeIdxs().get(2).equals(List.of(1, 3)));
+        assertEquals(List.of(1), info.getPathEdgeIdxs().get(3));
+        assertEquals(List.of(1, 5), info.getPathEdgeIdxs().get(4));
     }
 
     @Test
@@ -76,12 +76,12 @@ public class UnweightedShortPathTest {
         edges.add(new BasicEdge<Integer>(5, 4, 3, 1));
         edges.add(new BasicEdge<Integer>(6, 4, 5, 1));
         edges.add(new BasicEdge<Integer>(7, 2, 0, 1));
-        adj.add(Arrays.asList(0, 1, 7));
-        adj.add(Arrays.asList(0, 2));
-        adj.add(Arrays.asList(2, 3, 4, 7));
-        adj.add(Arrays.asList(1, 3, 5));
-        adj.add(Arrays.asList(4, 5, 6));
-        adj.add(Arrays.asList(6));
+        adj.add(List.of(0, 1, 7));
+        adj.add(List.of(0, 2));
+        adj.add(List.of(2, 3, 4, 7));
+        adj.add(List.of(1, 3, 5));
+        adj.add(List.of(4, 5, 6));
+        adj.add(List.of(6));
         graph.setAllNodes(nodes);
         graph.setAllEdges(edges);
         for (int i = 0; i < adj.size(); i++) {
@@ -89,16 +89,16 @@ public class UnweightedShortPathTest {
         }
         Pathfinder<Integer, Integer> pathfinder = new UnweightedShortPath<Integer>();
         PathInfoTriple<Integer> info = pathfinder.findAllSourcePaths(graph, 0);
-        assertEquals(Arrays.asList(0, 1, 2, 1, 3, 4), info.getPathCosts());
-        assertEquals(Arrays.asList(), info.getPathEdgeIdxs().get(0));
-        assertEquals(Arrays.asList(0), info.getPathEdgeIdxs().get(1));
-        assertTrue(info.getPathEdgeIdxs().get(2).equals(Arrays.asList(0, 2))
-                || info.getPathEdgeIdxs().get(2).equals(Arrays.asList(1, 3)));
-        assertEquals(Arrays.asList(1), info.getPathEdgeIdxs().get(3));
-        assertTrue(info.getPathEdgeIdxs().get(4).equals(Arrays.asList(1, 3, 4))
-                || info.getPathEdgeIdxs().get(4).equals(Arrays.asList(0, 2, 4)));
-        assertTrue(info.getPathEdgeIdxs().get(5).equals(Arrays.asList(0, 2, 4, 6))
-                || info.getPathEdgeIdxs().get(5).equals(Arrays.asList(1, 3, 4, 6)));
+        assertEquals(List.of(0, 1, 2, 1, 3, 4), info.getPathCosts());
+        assertEquals(List.of(), info.getPathEdgeIdxs().get(0));
+        assertEquals(List.of(0), info.getPathEdgeIdxs().get(1));
+        assertTrue(info.getPathEdgeIdxs().get(2).equals(List.of(0, 2))
+                || info.getPathEdgeIdxs().get(2).equals(List.of(1, 3)));
+        assertEquals(List.of(1), info.getPathEdgeIdxs().get(3));
+        assertTrue(info.getPathEdgeIdxs().get(4).equals(List.of(1, 3, 4))
+                || info.getPathEdgeIdxs().get(4).equals(List.of(0, 2, 4)));
+        assertTrue(info.getPathEdgeIdxs().get(5).equals(List.of(0, 2, 4, 6))
+                || info.getPathEdgeIdxs().get(5).equals(List.of(1, 3, 4, 6)));
     }
 
     @Test
@@ -116,11 +116,11 @@ public class UnweightedShortPathTest {
         edges.add(new BasicEdge<Integer>(3, 3, 2, 1));
         edges.add(new BasicEdge<Integer>(4, 2, 4, 1));
         edges.add(new BasicEdge<Integer>(5, 3, 4, 1));
-        adj.add(Arrays.asList(0));
-        adj.add(Arrays.asList(2));
-        adj.add(Arrays.asList(4));
-        adj.add(Arrays.asList(1, 3, 5));
-        adj.add(Arrays.asList());
+        adj.add(List.of(0));
+        adj.add(List.of(2));
+        adj.add(List.of(4));
+        adj.add(List.of(1, 3, 5));
+        adj.add(List.of());
         graph.setAllNodes(nodes);
         graph.setAllEdges(edges);
         for (int i = 0; i < adj.size(); i++) {
@@ -128,12 +128,12 @@ public class UnweightedShortPathTest {
         }
         Pathfinder<Integer, Integer> pathfinder = new UnweightedShortPath<Integer>();
         PathInfoTriple<Integer> info = pathfinder.findAllSourcePaths(graph, 0);
-        assertEquals(Arrays.asList(0, 1, 2, Integer.MAX_VALUE, 3), info.getPathCosts());
-        assertEquals(Arrays.asList(), info.getPathEdgeIdxs().get(0));
-        assertEquals(Arrays.asList(0), info.getPathEdgeIdxs().get(1));
-        assertEquals(Arrays.asList(0, 2), info.getPathEdgeIdxs().get(2));
-        assertEquals(Arrays.asList(-1), info.getPathEdgeIdxs().get(3));
-        assertEquals(Arrays.asList(0, 2, 4), info.getPathEdgeIdxs().get(4));
+        assertEquals(List.of(0, 1, 2, Integer.MAX_VALUE, 3), info.getPathCosts());
+        assertEquals(List.of(), info.getPathEdgeIdxs().get(0));
+        assertEquals(List.of(0), info.getPathEdgeIdxs().get(1));
+        assertEquals(List.of(0, 2), info.getPathEdgeIdxs().get(2));
+        assertEquals(List.of(-1), info.getPathEdgeIdxs().get(3));
+        assertEquals(List.of(0, 2, 4), info.getPathEdgeIdxs().get(4));
         assertEquals(Integer.MAX_VALUE, info.getInfinityValue());
     }
 
