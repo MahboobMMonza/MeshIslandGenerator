feef0c770738de66fec867dccfd829334a95e67b
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-08-20 17:07:09 -0400

    IS#1: Test uniqueness of all components in mesh
    Added tests for FixedMesh to ensure all components are unique in
    accordance to the definition of equality for those components.
    Also added safeguarding of null value tests (should not add null
    objects).

--- generator/src/test/java/ca/mcmaster/cas/se2aa4/a2/mesh/FixedMeshTest.java
+++ generator/src/test/java/ca/mcmaster/cas/se2aa4/a2/mesh/FixedMeshTest.java
@@ -15,91 +15,117 @@ import java.util.List;
 
 public class FixedMeshTest {
 
-    // Test to check if the polygon can't be duplicated
-    @Test
-    public void addPolyTest() {
-        Mesh mesh = new FixedMesh();
-        Polygon poly = new Polygon();
-        poly.setCentroid(2, 2);
-        assertTrue(mesh.addPoly(poly));
-    }
-
-    // Test to check if the polygon is added and can't be duplicated
     @Test
     public void addNewPolySameCentroidTest() {
+        // Test to check if the polygon is added and can't be duplicated
         Mesh mesh = new FixedMesh();
-        Polygon poly = new Polygon(3, 5);
-        Polygon otherPoly = new Polygon(3, 5);
-        assertTrue(mesh.addPoly(poly));
-        assertFalse(mesh.addPoly(otherPoly));
+        Poly poly = new Polygon(3, 5);
+        Poly otherPoly = new Polygon(3, 5);
+        assertAll("Assert that only one unique Poly is ever added",
+                () -> assertTrue(mesh.addPoly(poly)),
+                () -> assertFalse(mesh.addPoly(otherPoly)),
+                () -> assertFalse(mesh.addPoly(poly)));
     }
 
-    // Test to check if the segment is added and can't be duplicated
     @Test
     public void addSegTest() {
+        // Test to check if the segment is added and can't be duplicated
         Mesh mesh = new FixedMesh();
-        Segment seg = new Segment(1, 1, 2, 2);
-        assertTrue(mesh.addSeg(seg));
-        assertFalse(mesh.addSeg(seg));
+        Seg seg = new Segment(1, 1, 2, 2);
+        Seg otherSeg = new Segment(1, 1, 2, 2);
+        assertAll("Assert that only one unique Seg is ever added",
+                () -> assertTrue(mesh.addSeg(seg)),
+                () -> assertFalse(mesh.addSeg(otherSeg)),
+                () -> assertFalse(mesh.addSeg(seg)));
     }
 
-    // Test to check if the vertex is added and can't be duplicated
     @Test
     public void addVertTest() {
+        // Test to check if the vertex is added and can't be duplicated
         Mesh mesh = new FixedMesh();
-        Vertex v = new Vertex();
-        assertTrue(mesh.addVert(v));
-        assertFalse(mesh.addVert(v));
+        Vert vertex = new Vertex(-4.23, 12.3432);
+        Vert otherVertex = new Vertex(-4.23, 12.3432);
+        assertAll("Assert that only one unique Vert is ever added",
+                () -> assertTrue(mesh.addVert(vertex)),
+                () -> assertFalse(mesh.addVert(otherVertex)),
+                () -> assertFalse(mesh.addVert(vertex)));
     }
 
-    // Test to check if the any new polygons won't be added after mesh.lock()
     @Test
     public void lockTest() {
+        // Test to check if the any new objects won't be added after mesh.lock()
         Mesh mesh = new FixedMesh();
-        Polygon poly = new Polygon();
+        Poly poly = new Polygon(54.564, 54632.2454);
+        Vert vert = new Vertex(42.587, 42320.2112);
+        Seg seg = new Segment(-2288.1541, 46645.112, 23, -28722);
+        Poly poly2 = new Polygon(-3, -45);
         assertTrue(mesh.addPoly(poly));
         mesh.lock();
-        Polygon poly2 = new Polygon();
-        assertFalse(mesh.addPoly(poly2));
+        assertAll("Assert that no other objects can be added to any category after locking",
+                () -> assertFalse(mesh.addPoly(poly2)),
+                () -> assertFalse(mesh.addVert(vert)),
+                () -> assertFalse(mesh.addSeg(seg)),
+                () -> assertFalse(mesh.addPoly(null)),
+                () -> assertFalse(mesh.addVert(null)),
+                () -> assertFalse(mesh.addSeg(null)));
     }
 
-    // Test to check if the right polygon is returned
     @Test
     public void getPolyTest() {
+        // Test to check if the correct polygon is returned
         Mesh mesh = new FixedMesh();
-        Polygon poly = new Polygon();
+        Poly poly, poly2;
+        poly = new Polygon();
+        poly2 = new Polygon();
         poly.setCentroid(2, 2);
+        poly2.setCentroid(2, 3);
         mesh.addPoly(poly);
+        mesh.addPoly(poly2);
+        mesh.addPoly(poly2);
         mesh.addPoly(poly);
         mesh.lock();
         List<Poly> polygons = mesh.getPolys();
-        assertEquals(1, polygons.size());
+        assertAll("Assert that getPolys returns a list with unique polygons",
+                () -> assertEquals(2, polygons.size()),
+                () -> assertEquals(polygons.get(0), poly),
+                () -> assertEquals(polygons.get(1), poly2));
     }
 
-    // Test to check if the right segment is added
     @Test
     public void getSegTest() {
+        // Test to check if the correct segment is added
         Mesh mesh = new FixedMesh();
-        Segment seg = new Segment(1, 1, 2, 2);
-        Segment seg2 = new Segment(3, 3, 4, 4);
+        Seg seg = new Segment(1, 1, 2, 2);
+        Seg seg2 = new Segment(3, 3, 4, 4);
+        mesh.addSeg(seg2);
         mesh.addSeg(seg);
         mesh.addSeg(seg);
         mesh.addSeg(seg2);
         mesh.lock();
         List<Seg> segments = mesh.getSegs();
-        assertEquals(2, segments.size());
+        assertAll("Assert that getSegs returns a list with unique segments",
+                () -> assertEquals(2, segments.size()),
+                () -> assertEquals(segments.get(0), seg),
+                () -> assertEquals(segments.get(1), seg2));
     }
 
-    // Test to check if the right vertex is added
     @Test
     public void getVertTest() {
+        // Test to check if the correct vertex is added
         Mesh mesh = new FixedMesh();
-        Vertex v = new Vertex(2, 2);
-        mesh.addVert(v);
-        mesh.addVert(v);
+        Vert vert, vert2;
+        vert = new Vertex(2, 2);
+        vert2 = new Vertex(-1, 3);
+        mesh.addVert(vert);
+        mesh.addVert(vert);
+        mesh.addVert(vert2);
+        mesh.addVert(vert2);
         mesh.lock();
         List<Vert> vertices = mesh.getVerts();
-        assertEquals(1, vertices.size());
+        assertAll("Assert that getVerts returns a list with unique vertices",
+                () -> assertEquals(2, vertices.size()),
+                () -> assertEquals(vertices.get(0), vert2),
+                () -> assertEquals(vertices.get(1), vert));
     }
 
 }
